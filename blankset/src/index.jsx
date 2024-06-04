ee26e6ecc0b7484e63d0023ba557d99d3c9f7e75
Elysse Viena <elysseviena23@gmail.com>
2024-06-04 16:14:16 -0400

    the proper index added

--- blankset/src/index.jsx
+++ blankset/src/index.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {getAllPizza} from "../public/data.js";
+
+const pizzaData = getAllPizza();
+console.log(pizzaData); //just for debugging
 
 function App() {
     return <>
         <h1>Hello React!</h1>
+        <Pizza />
         </>;
 }
 
+function Pizza(){
+    return(
+        <>
+            <h2>Pizza time</h2>
+            {pizzaData.map(
+                    pizItem =>{
+                        return(
+                            <>
+                                <img src={`../public/${pizItem.photoName}`} alt={`A picture of a ${pizItem.name}`}/>
+                                <h2>{pizItem.name}</h2>
+                                <p>{pizItem.ingredients}</p>
+                            </>
+                        );
+                    }
+                )
+            }
+        </>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
