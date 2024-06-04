6b1e500f36de0b19a1ce4a3c6ede8f9ce15871d1
Elysse Viena <elysseviena23@gmail.com>
2024-06-04 18:43:38 -0400

    added header, footer and menu components.

--- blankset/src/index.jsx
+++ blankset/src/index.jsx
@@ -7,23 +7,62 @@ console.log(pizzaData); //just for debugging
 
 function App() {
     return <>
-        <h1>Hello React!</h1>
-        <Pizza />
+        <Header />
+        <Menu />
+        <Footer />
         </>;
 }
+
+//i was only just informed I could've been using arrow functions. so uh. heres to the sudden change
+const Header = () => {
+    return(
+        <>
+            <h1>Fast React Pizza Co.</h1>
+        </>
+    );
+};
+
+//moved pizza component to render here
+const Menu = () => {
+    return(
+        <>
+            <h2>Our Menu</h2>
+            <Pizza />
+        </>
+    );
+};
+
+const Footer = () => {
+    let hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    let availabilityMsg = "There was an error fetching the hours.";
+    //now we can just put any js in here lol
+
+    if(hour >= openHour && hour < closeHour){
+        availabilityMsg = "We're currently open!";
+    }else{
+        availabilityMsg = "We're currently closed.";
+    }
+
+    return(
+        <>
+            <footer>The time is now {new Date().toLocaleTimeString()}. {availabilityMsg} </footer>
+        </>
+    );
+}; //yes i add semicolons to everything. Its easier to debug this way
 
 function Pizza(){
     return(
         <>
-            <h2>Pizza time</h2>
             {pizzaData.map(
                     pizItem =>{
                         return(
                             <>
-                                <img src={`../public/${pizItem.photoName}`} alt={`A picture of a ${pizItem.name}`}/>
+                                <img src={`${pizItem.photoName}`} alt={`A picture of a ${pizItem.name}`}/>
                                 <h2>{pizItem.name}</h2>
                                 <p>{pizItem.ingredients}</p>
-                            </>
+                            </> //i couldnt tell you why but watch this like a hawk. it keeps trying to become some other thing like footer or header. anything but a fragment
                         );
                     }
                 )
